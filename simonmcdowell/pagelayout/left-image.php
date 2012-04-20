926dabc602f24fac770826dafff984aa4007c859
Sotaro Sugimoto <madeoftheforce@gmail.com>
2012-04-20 21:08:52 +1200

    add href and icon for band

--- simonmcdowell/pagelayout/left-image.php
+++ simonmcdowell/pagelayout/left-image.php
@@ -4,14 +4,19 @@
 		</div>
     <div class="social-icon">
     <img src="images/layout/facebook.gif" width="40" height="40" class="icon" />
-    <a class="css3button"><span class="inner-text">Simon's facebook</span></a>
+    <a class="css3button" href="http://www.facebook.com/simonmcdowelldrummer" ><span class="inner-text">Simon's facebook</span></a>
     </div>
     <div  class="social-icon">
     <img src="images/layout/youtube.gif" width="40" height="40" class="icon" />
-    <a class="css3button"><span class="inner-text">Simon's youtube</span></a>
+    <a class="css3button" href="http://www.youtube.com/user/themoaspecial" ><span class="inner-text">Simon's youtube</span></a>
     </div>
     <div  class="social-icon">
     <img src="images/layout/myspace.gif" width="40" height="40" class="icon" />
-    <a class="css3button"><span class="inner-text">Simon's myspace</span></a>
+    <a class="css3button" href="http://www.myspace.com/simonmcdowelldrums" ><span class="inner-text">Simon's myspace</span></a>
+		</div>
+		<hr width="160" align="left" />
+    <div class="social-icon">
+    <img src="images/layout/facebook.gif" width="40" height="40" class="icon" />
+    <a class="css3button" href="http://www.facebook.com/favoriteunderdog" ><span class="inner-text">Favorite Underdog</span></a>
     </div>
 </div>
