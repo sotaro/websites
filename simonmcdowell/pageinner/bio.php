2580287bc54cb832c5ff69ddd27c41d164e23847
Sotaro Sugimoto <madeoftheforce@gmail.com>
2012-07-12 11:58:47 +1200

    hmm

--- simonmcdowell/pageinner/bio.php
+++
@@ -1,7 +0,0 @@
-<h2>Bio</h2>
-<div id="tabs">
-<ul>
-<li><a href="admin/bio_en.txt">English</a></li>
-<li><a href="admin/bio_jp.txt">Japanese</a></li>
-</ul>
-</div>
--- simonmcdowell/pagelayout/footer.php
+++
@@ -1,9 +0,0 @@
-<?php $y=date("Y");
-echo '<div id="footer">Copyright &copy; 2007-'.$y.' Simon McDowell, all rights reserved.</div>';
-?>
-<script src="http://www.google-analytics.com/urchin.js" type="text/javascript">
-</script>
-<script type="text/javascript">
-_uacct = "UA-462542-4";
-urchinTracker();
-</script>
